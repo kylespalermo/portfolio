5c99fe735dfc726bba68a4e373989a6372bc75bd
kylespalermo <61397896+kylespalermo@users.noreply.github.com>
2022-07-06 14:35:56 -0400

    misc fixes/improvements

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import DOMPurify from 'dompurify';
-
+import React, {useState} from 'react';
 
 const Intro = (props) => {
-    var cleanIntroLinks = DOMPurify.sanitize(props.introGet.links.html);
+    const [contactText, updateContactText] = useState("Contact");
+
     return (
         <section className="intro">
             <h1>About Me</h1>
             <div className="intro-text">
                 <p>{props.introGet.introText}</p>
-                <p className="intro-links" dangerouslySetInnerHTML={{__html:cleanIntroLinks}}/>
+                <ul className="intro-links">
+                    <li>< a href="https://www.linkedin.com/in/kylepalermo/">LinkedIn</a></li>
+                    <li><a href="https://github.com/kylespalermo">GitHub</a></li>
+                    <li><a href={props.introGet.resume.url}>Resume</a></li>
+                    <li onClick={() => updateContactText("kyle@kylepalermo.com")}>{contactText}</li>
+                </ul>
             </div>
             <img src={props.introGet.photo.url} alt="My partner and I at Brooklyn Botanical Garden" />
         </section>
